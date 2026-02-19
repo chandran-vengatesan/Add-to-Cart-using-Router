d678b438af8b6c3ea34073b1be8b691f7f2d9edb
chandran <chandranpv92@gmail.com>
2026-02-19 21:41:08 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ cartCount }) => {
   return (
     <nav className="bg-white shadow-md p-4 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold text-blue-600">Guvi Store</Link>
+        <Link to="/" className="text-2xl font-bold text-blue-600">Thugi Store</Link>
         <Link to="/cart">
           <button className="bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-6 rounded-lg transition flex items-center gap-2">
             <FiShoppingCart size={20} />
@@ -20,3 +20,4 @@ const Navbar = ({ cartCount }) => {
 
 
 export default Navbar;
+
